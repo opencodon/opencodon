NousResearch/hermes-agent

fix(vertex,moa): register vertex in PROVIDER_REGISTRY and HERMES_OVERLAYS
The Vertex AI provider (added same-day, commit c73e74386) was never added to
either of the two provider registries that agent/auxiliary_client.py and the
MoA slot-resolution chain depend on, breaking Vertex outside the main
conversation loop:

1. hermes_cli/auth.py::PROVIDER_REGISTRY had no "vertex" entry. The
   plugin-auto-extend loop that normally fills gaps explicitly skips
   non-api_key auth types (`if _pp.auth_type != "api_key": continue`), and
   Vertex was never hand-declared like "bedrock" is. Because
   resolve_provider_client() in agent/auxiliary_client.py gates everything
   on `pconfig = PROVIDER_REGISTRY.get(provider)` and returns (None, None)
   immediately when pconfig is None, its `elif pconfig.auth_type == "vertex"`
   branch was permanently dead code — every auxiliary Vertex call (vision,
   title generation, reflection, context compression, MoA reference/
   aggregator slots) failed outright, not just a MoA-specific edge case.

2. hermes_cli/providers.py::HERMES_OVERLAYS also had no "vertex" entry, so
   hermes_cli.providers.get_provider("vertex") returned None. This backs
   _preserve_provider_with_base_url() in agent/auxiliary_client.py, which a
   MoA slot's resolved (base_url, api_key) pair needs to keep its "vertex"
   identity instead of silently collapsing to "custom" — losing the
   identity _refresh_provider_credentials() needs to re-mint an expired
   OAuth2 token (~1h lifetime) on a 401, and permanently breaking every
   subsequent call in that MoA preset for the rest of the session.

Fix mirrors the existing "bedrock"/aws_sdk entries in both registries
exactly, plus adds a "vertex" branch to _refresh_provider_credentials() (it
had branches for openai-codex/nous/anthropic/xai-oauth but not vertex,
so a 401 fell through to `return False` without evicting the stale cached
client).

- hermes_cli/auth.py: hand-declared vertex ProviderConfig(auth_type="vertex")
  in 

tests/hermes_cli/test_vertex_provider.py
@@ -98,3 +98,31 @@ def test_vertex_extra_body_empty_without_reasoning():
 
     p = get_provider_profile("vertex")
     assert p.build_extra_body(model="google/gemini-3-flash-preview") == {}
+
+
+def test_vertex_registered_in_provider_registry():
+    """PROVIDER_REGISTRY (hermes_cli.auth) is what agent/auxiliary_client.py's
+    resolve_provider_client() looks up before dispatching on auth_type. Without
+    an entry here, the ``elif pconfig.auth_type == "vertex":`` branch there is
+    unreachable dead code — every auxiliary Vertex call (vision, title
+    generation, MoA reference/aggregator slots, ...) fails at the
+    ``pconfig is None`` guard before ever reaching it."""
+    from hermes_cli.auth import PROVIDER_REGISTRY
+
+    cfg = PROVIDER_REGISTRY.get("vertex")
+    assert cfg is not None
+    assert cfg.auth_type == "vertex"
+
+
+def test_vertex_registered_in_hermes_overlays():
+    """hermes_cli.providers.get_provider("vertex") backs
+    _preserve_provider_with_base_url() in agent/auxiliary_client.py, which
+    decides whether a MoA slot's resolved Vertex (base_url, api_key) pair
+    keeps its "vertex" provider identity or silently collapses to "custom" —
+    losing the identity _refresh_provider_credentials() needs to re-mint an
+    expired OAuth2 token on a 401."""
+    from hermes_cli.providers import get_provider
+
+    resolved = get_provider("vertex")
+    assert resolved is not None
+    assert resolved.auth_type == "vertex"

opencodon/opencodon

(cherry picked from commit 3ea35d671106e586b51aa17ad0f4d162467b92a0)

tests/opencodon_cli/test_vertex_provider.py
@@ -98,3 +98,31 @@ def test_vertex_extra_body_empty_without_reasoning():
 
     p = get_provider_profile("vertex")
     assert p.build_extra_body(model="google/gemini-3-flash-preview") == {}
+
+
+def test_vertex_registered_in_provider_registry():
+    """PROVIDER_REGISTRY (opencodon_cli.auth) is what agent/auxiliary_client.py's
+    resolve_provider_client() looks up before dispatching on auth_type. Without
+    an entry here, the ``elif pconfig.auth_type == "vertex":`` branch there is
+    unreachable dead code — every auxiliary Vertex call (vision, title
+    generation, MoA reference/aggregator slots, ...) fails at the
+    ``pconfig is None`` guard before ever reaching it."""
+    from opencodon_cli.auth import PROVIDER_REGISTRY
+
+    cfg = PROVIDER_REGISTRY.get("vertex")
+    assert cfg is not None
+    assert cfg.auth_type == "vertex"
+
+
+def test_vertex_registered_in_hermes_overlays():
+    """opencodon_cli.providers.get_provider("vertex") backs
+    _preserve_provider_with_base_url() in agent/auxiliary_client.py, which
+    decides whether a MoA slot's resolved Vertex (base_url, api_key) pair
+    keeps its "vertex" provider identity or silently collapses to "custom" —
+    losing the identity _refresh_provider_credentials() needs to re-mint an
+    expired OAuth2 token on a 401."""
+    from opencodon_cli.providers import get_provider
+
+    resolved = get_provider("vertex")
+    assert resolved is not None
+    assert resolved.auth_type == "vertex"