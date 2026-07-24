NousResearch/hermes-agent

fix(moa): route Copilot slots by target model

tests/hermes_cli/test_copilot_model_api_mode.py
@@ -1,0 +1,23 @@
+"""Tests for Copilot model API-mode routing."""
+
+from __future__ import annotations
+
+
+def test_copilot_claude_stays_on_chat_completions_even_if_catalog_lists_messages():
+    from hermes_cli.models import copilot_model_api_mode
+
+    catalog = [
+        {
+            "id": "claude-opus-4.8",
+            "supported_endpoints": ["/v1/messages"],
+        }
+    ]
+
+    assert copilot_model_api_mode("claude-opus-4.8", catalog=catalog) == "chat_completions"
+
+
+def test_copilot_gpt5_still_uses_responses_api():
+    from hermes_cli.models import copilot_model_api_mode
+
+    assert copilot_model_api_mode("gpt-5.5", catalog=[]) == "codex_responses"
+    assert copilot_model_api_mode("gpt-5-mini", catalog=[]) == "chat_completions"

tests/hermes_cli/test_copilot_runtime_api_mode.py
@@ -1,0 +1,40 @@
+"""Regression tests for Copilot runtime api_mode resolution."""
+
+from __future__ import annotations
+
+
+def test_copilot_runtime_api_mode_uses_target_model_over_stale_config_default(monkeypatch):
+    """MoA/fallback slots must derive Copilot api_mode from the slot model.
+
+    Repro: user's main/default Copilot model is GPT-5.5 (Responses API), but a
+    MoA reference slot is Copilot Claude Opus (Chat Completions). Runtime
+    resolution previously looked only at model.default and returned
+    codex_responses for the Claude slot, causing Copilot to reject it with
+    "model ... does not support Responses API".
+    """
+    from hermes_cli import runtime_provider as rp
+
+    monkeypatch.setattr(
+        "hermes_cli.models.copilot_model_api_mode",
+        lambda model, api_key=None: "codex_responses" if str(model).startswith("gpt-5") else "chat_completions",
+    )
+
+    assert rp._copilot_runtime_api_mode(
+        {"provider": "copilot", "default": "gpt-5.5"},
+        "token",
+        target_model="claude-opus-4.8",
+    ) == "chat_completions"
+
+
+def test_copilot_runtime_api_mode_still_uses_default_without_target(monkeypatch):
+    from hermes_cli import runtime_provider as rp
+
+    monkeypatch.setattr(
+        "hermes_cli.models.copilot_model_api_mode",
+        lambda model, api_key=None: "codex_responses" if str(model).startswith("gpt-5") else "chat_completions",
+    )
+
+    assert rp._copilot_runtime_api_mode(
+        {"provider": "copilot", "default": "gpt-5.5"},
+        "token",
+    ) == "codex_responses"

opencodon/opencodon

(cherry picked from commit 02f1dd08579727fcf47a867e1a0c157bffaa561b)

tests/opencodon_cli/test_copilot_model_api_mode.py
@@ -1,0 +1,23 @@
+"""Tests for Copilot model API-mode routing."""
+
+from __future__ import annotations
+
+
+def test_copilot_claude_stays_on_chat_completions_even_if_catalog_lists_messages():
+    from opencodon_cli.models import copilot_model_api_mode
+
+    catalog = [
+        {
+            "id": "claude-opus-4.8",
+            "supported_endpoints": ["/v1/messages"],
+        }
+    ]
+
+    assert copilot_model_api_mode("claude-opus-4.8", catalog=catalog) == "chat_completions"
+
+
+def test_copilot_gpt5_still_uses_responses_api():
+    from opencodon_cli.models import copilot_model_api_mode
+
+    assert copilot_model_api_mode("gpt-5.5", catalog=[]) == "codex_responses"
+    assert copilot_model_api_mode("gpt-5-mini", catalog=[]) == "chat_completions"

tests/opencodon_cli/test_copilot_runtime_api_mode.py
@@ -1,0 +1,40 @@
+"""Regression tests for Copilot runtime api_mode resolution."""
+
+from __future__ import annotations
+
+
+def test_copilot_runtime_api_mode_uses_target_model_over_stale_config_default(monkeypatch):
+    """MoA/fallback slots must derive Copilot api_mode from the slot model.
+
+    Repro: user's main/default Copilot model is GPT-5.5 (Responses API), but a
+    MoA reference slot is Copilot Claude Opus (Chat Completions). Runtime
+    resolution previously looked only at model.default and returned
+    codex_responses for the Claude slot, causing Copilot to reject it with
+    "model ... does not support Responses API".
+    """
+    from opencodon_cli import runtime_provider as rp
+
+    monkeypatch.setattr(
+        "opencodon_cli.models.copilot_model_api_mode",
+        lambda model, api_key=None: "codex_responses" if str(model).startswith("gpt-5") else "chat_completions",
+    )
+
+    assert rp._copilot_runtime_api_mode(
+        {"provider": "copilot", "default": "gpt-5.5"},
+        "token",
+        target_model="claude-opus-4.8",
+    ) == "chat_completions"
+
+
+def test_copilot_runtime_api_mode_still_uses_default_without_target(monkeypatch):
+    from opencodon_cli import runtime_provider as rp
+
+    monkeypatch.setattr(
+        "opencodon_cli.models.copilot_model_api_mode",
+        lambda model, api_key=None: "codex_responses" if str(model).startswith("gpt-5") else "chat_completions",
+    )
+
+    assert rp._copilot_runtime_api_mode(
+        {"provider": "copilot", "default": "gpt-5.5"},
+        "token",
+    ) == "codex_responses"